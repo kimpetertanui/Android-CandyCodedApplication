608ae03bc7ddf8d4751c0dd0e20f11828f178c14
kimpetertanui <kimelipeter526@gmail.com>
2020-09-24 14:55:16 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,13 +1,18 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
+import android.icu.text.IDNA;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
+
+   Uri Uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,4 +35,15 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createMapIntent(View view){
+     Uri=   Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent=new Intent(Intent.ACTION_VIEW,Uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) !=null){
+            startActivity(mapIntent);
+        }
+
+    }
 }
